5109ab97ca756af7d92250a363f406800767afbd
Dario <dariomrkonjic1@gmail.com>
2023-03-02 21:59:39 +0100

    Add getEnvData, getUrl and getKey.

---
+++ js/utils.js
@@ -0,0 +1,18 @@
+/**
+ * Gets the environment data.
+ * @returns {Promise<{baseUrl: string, apiKey: string}>} Env data.
+ */
+export const getEnvData = async () => await (await fetch("../env.json")).json();
+
+/**
+ * Gets the full url.
+ * @param {string} route api route.
+ * @returns {string} full url.
+ */
+export const getUrl = async route => (await getEnvData()).baseUrl + route;
+
+/**
+ * Gets the api key.
+ * @returns {string} api key.
+ */
+export const getKey = async () => (await getEnvData()).apiKey;
